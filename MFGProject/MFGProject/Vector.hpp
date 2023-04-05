61a0ea6337c2c60b54a41d4aa2e32204fcdd0660
BleuSky03 <taksin@photomann.com>
2023-04-05 13:40:24 -0400

    Comments

--- MFGProject/MFGProject/Vector.hpp
+++ MFGProject/MFGProject/Vector.hpp
@@ -12,13 +12,17 @@ public:
     y = y_t;
   }
 
-  /// @brief scales this vector by a provided scalar
+  /// @brief scales this vector2 by a provided scalar
   /// @param scalar the value to scale by
   void Scale(float scalar) {
     x *= scalar;
     y *= scalar;
   }
 
+  /// @brief calculates the dot product of two given vector2s
+  /// @param vect1 the first vector2
+  /// @param vect2 the second vector2
+  /// @return a float of the calculated dot product
   float DotProd(Vector2 vect1, Vector2 vect2) {
     return (vect1.x * vect2.x) + (vect1.y * vect2.y); 
   }
