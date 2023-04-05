9f1716c6535ca0e8fc658bb948a793eb3fe47e1f
Connor Saila <csaila@live.com>
2023-04-05 13:36:44 -0400

    comments

--- MFGProject/MFGProject/Vector.hpp
+++ MFGProject/MFGProject/Vector.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+/// @brief represents a 2D vector
 class Vector2 {
 private:
   int x;
@@ -11,8 +12,10 @@ public:
     y = y_t;
   }
 
-  Vector2 Scale(float scalar) {
-    this->x *= scalar;
-    this->y *= scalar;
+  /// @brief scales this vector by a provided scalar
+  /// @param scalar the value to scale by
+  void Scale(float scalar) {
+    x *= scalar;
+    y *= scalar;
   }
 };
